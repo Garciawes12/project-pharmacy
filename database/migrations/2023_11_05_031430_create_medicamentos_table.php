e3e48406bd02ef477f60d11f94442accc6577da3
Garciawes12 <wesleyjosue47@gmail,com>
2023-11-04 21:42:19 -0600

    update

---
+++ database/migrations/2023_11_05_031430_create_medicamentos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicamentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('imagen')->nullable();
+            $table->string('nombre');
+            $table->string('categoria');
+            $table->string('proveedor');
+            $table->decimal('precio_compra', 8, 2);
+            $table->decimal('precio_venta', 8, 2);
+            $table->date('fecha_vencimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicamentos');
+    }
+};
